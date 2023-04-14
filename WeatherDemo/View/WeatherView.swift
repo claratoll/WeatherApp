8a4ff1fd1bece222494e351f0c86876b62a66789
claratoll <clara.toll@gmail.com>
2023-04-14 14:17:46 +0200

    first commit

---
+++ WeatherDemo/View/WeatherView.swift
@@ -0,0 +1,93 @@
+//
+//  WeatherView.swift
+//  WeatherDemo
+//
+//  Created by Clara Toll on 2023-04-14.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    
+    var body: some View {
+        ZStack(alignment: .leading){
+            VStack{
+                VStack(alignment: .leading, spacing: 5){
+                    Text(weather.name)
+                        .bold().font(.title)
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                
+                VStack{
+                    HStack{
+                        VStack(spacing: 20){
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    Spacer ()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://easydrawingguides.com/wp-content/uploads/2020/10/New-York-step-10.png")) {image in image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+            }
+            
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            
+            VStack{
+                Spacer()
+                VStack(alignment: .leading, spacing: 20){
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    HStack{
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + "*"))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + "*"))                    }
+                    HStack{
+                        WeatherRow(logo: "wind", name: "Wind speed", value: (weather.wind.speed.roundDouble() + "m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: (weather.main.humidity.roundDouble() + "%"))                    }                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.685, saturation: 0.853, brightness: 0.26))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.685, saturation: 0.853, brightness: 0.26))
+        .preferredColorScheme(.dark)
+        
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Clara Toll on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
